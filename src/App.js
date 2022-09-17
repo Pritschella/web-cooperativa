c246ff7369481bf9ad12dd718c2a24734f749f1b
Pritschella <44498987+Pritschella@users.noreply.github.com>
2022-09-16 23:36:01 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navegacion/Navbar';
+import Principal from './Components/Paginas/principal';
+import InicioSesion from './Components/Paginas/inicioSesion';
+import Carrito from './Components/Paginas/carrito';
+
+function App() {
+
+    return (
+
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<Principal/>} />
+                    <Route exact path="/inicioSesion" element={<InicioSesion/>} />
+                    <Route  path="/carrito" element={<Carrito/>} />
+                </Routes>
+
+                <carousel/>
+
+            </Router>
+
+
+    )
+
+}
+
+export default App;
---
+++ src/Components/Paginas/principal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from '../Carrusel/Carousel';
+
+const Principal = () => {
+    return(
+        <div>
+            <Carousel/>
+            <h1>Principal</h1>
+
+        </div>
+    );
+};
+
+export default Principal;
