3e3bd6beb8543011060e3681225e9a7da01bbdd4
Pritschella <44498987+Pritschella@users.noreply.github.com>
2022-09-17 00:17:01 -0500

    Commit: "Se modificó pagina principal"

--- src/Components/Carrusel/Carousel.js
+++ src/Components/Carrusel/Carousel.js
@@ -4,45 +4,65 @@ import React from "react"
 const carousel = () => {
     return (
         <div>
-            
-            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
+            <section data-bs-version="5.1" className="features4 cid-t0MKDXchPe" id="features4-1">
+    
+    
+    <div className="container">
+        <div className="mbr-section-head">
+            <h4 className="mbr-section-title mbr-fonts-style align-center mb-0 display-2">
+                <strong>Lo más vendido</strong></h4>
+            <h5 className="mbr-section-subtitle mbr-fonts-style align-center mb-0 mt-2 display-5"></h5>
+        </div>
+        <div className="row mt-4">
+            <div class="item features-image сol-12 col-md-6 col-lg-4">
+                <div className="item-wrapper">
+                    <div className="item-img">
+                        <img src="assets/images/desayunos-299x168.jpg" alt="Mobirise Website Builder" title=""/>
+                    </div>
+                    <div className="item-content">
+                        <h5 className="item-title mbr-fonts-style display-5"><strong>Desayunos</strong></h5>
+                        <h6 className="item-subtitle mbr-fonts-style mt-1 display-7"></h6>
+                        <p className="mbr-text mbr-fonts-style mt-3 display-7">En el apartado de desayunos se encuentra el variado menú de comida que puedes encontrar dentro de la cooperativa escolar.</p>
+                    </div>
+                    <div className="mbr-section-btn item-footer mt-2"><a href="desayunos.html" className="btn item-btn btn-black display-7">Ir a comprar</a></div>
                 </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active" data-bs-interval="10000">
-                    <img src="./images/bebidas.jpg" className="d-block w-100" alt="..."/>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                    <img src="./images/desayunos.jpg" class="d-block w-100" alt="..."/>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="./images/sabritas.png" class="d-block w-100" alt="..."/>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
             </div>
+            <div className="item features-image сol-12 col-md-6 col-lg-4">
+                <div className="item-wrapper">
+                    <div className="item-img">
+                        <img src="assets/images/sabritas-512x294.png" alt="Mobirise Website Builder" title=""/>
+                    </div>
+                    <div className="item-content">
+                        <h5 className="item-title mbr-fonts-style display-5"><strong>Botanas</strong></h5>
+                        <h6 className="item-subtitle mbr-fonts-style mt-1 display-7"></h6>
+                        <p className="mbr-text mbr-fonts-style mt-3 display-7">En el apartado botanas se encuentran los diferentes productos de botanas, como lo son productos de sabritas, barcel, entre otros.</p>
+                    </div>
+                    <div className="mbr-section-btn item-footer mt-2"><a href="botanas.html" className="btn item-btn btn-black display-7">Ir a comprar</a></div>
+                </div>
+            </div>
+            <div className="item features-image сol-12 col-md-6 col-lg-4">
+                <div className="item-wrapper">
+                    <div className="item-img">
+                        <img src="assets/images/bebidas-275x183.jpg" alt="Mobirise Website Builder" title=""/>
+                    </div>
+                    <div className="item-content">
+                        <h5 className="item-title mbr-fonts-style display-5"><strong>Bebidas</strong></h5>
+                        <h6 className="item-subtitle mbr-fonts-style mt-1 display-7"></h6>
+                        <p className="mbr-text mbr-fonts-style mt-3 display-7">En el apartador bebidas se encuentran los productos relacionados con productos coca -cola... (pendiente)<br/></p>
+                    </div>
+                    <div className="mbr-section-btn item-footer mt-2"><a href="bebidas.html" className="btn item-btn btn-black display-7">Ir a comprar</a></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+            
+
+
+
+
 
         </div>
     )
--- src/Components/Navegacion/Navbar.js
+++ src/Components/Navegacion/Navbar.js
@@ -10,6 +10,7 @@ const navbar = () => {
                     <Link to="/">
                         <img src="./logo_colegio.jpg" width="30"/>
                     </Link>
+                    <h3>CDMM</h3>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
