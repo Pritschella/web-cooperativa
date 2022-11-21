9ce6e9eb379a290db669d7f0be8f04d9a84f393c
Pritschella <44498987+Pritschella@users.noreply.github.com>
2022-11-21 17:00:44 -0600

    Commit: "Se modificó diseño"

--- src/Components/Navegacion/Navbar.js
+++ src/Components/Navegacion/Navbar.js
@@ -10,20 +10,20 @@ const navbar = () => {
                     <Link to="/">
                         <img src='./logo_colegio.jpg' width="30" alt="."/>
                     </Link>
-                    <h3>CDMM</h3>
+                    <h3> <strong> Cafetería CDMM Jerez </strong></h3>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav mx-auto">
                         <li className="nav-item">
-                        <Link className="nav-link" to="/">Principal</Link>
+                        <Link className="nav-link" to="/"><strong>Principal</strong></Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/inicioSesion">Inicio Sesion</Link>
+                        <Link className="nav-link" to="/inicioSesion"><strong>Inicio Sesion</strong></Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/carrito">Carrito</Link>
+                        <Link className="nav-link" to="/carrito"><strong>Carrito</strong></Link>
                         </li>
                     </ul>
                     </div>
